3226c68f4161264395bac84334c3f101f4782b1c
Prashant Shinde <prashantsshinde2604@gmail.com>
2024-07-25 16:53:43 +0530

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,15 @@
+const resumeBtn = document.querySelector("#viewResumeButton");
+
+// resumeBtn.onclick = ()=>{
+//     window.open("https://drive.google.com/file/d/1e21ueYGnk5iwn88tFeywiy6knhU0KaUY/view?usp=sharing", '_blank');
+// }
+
+resumeBtn.addEventListener("click", openResume);
+
+
+const resumeDownloadBtn = document.querySelector("#resume-button-2");
+resumeDownloadBtn.addEventListener("click", openResume);
+
+function openResume(){
+    window.open("", '_blank');
+}
---
+++ js/projects.js
@@ -0,0 +1,54 @@
+let onlineShoppingSrcBtn = document.querySelector("#PluralSight---clone_source_code_btn");
+onlineShoppingSrcBtn.onclick = () =>{
+    window.open("https://github.com/Prashant5300/PluralSight---clone", '_blank');
+}
+
+let onlineShoppingLiveBtn = document.querySelector("#PluralSight---clone_live_btn");
+onlineShoppingLiveBtn.onclick = () =>{
+    window.open("https://dulcet-brigadeiros-75a972.netlify.app/", '_blank');
+}
+
+let covidProofSrcBtn = document.querySelector("#YouTube-Clone_source_code_btn");
+covidProofSrcBtn.onclick = () =>{
+    window.open("https://github.com/Prashant5300/YouTube-Clone", '_blank');
+}
+
+let covidProofLiveBtn = document.querySelector("#YouTube-Clone_live_btn");
+covidProofLiveBtn.onclick = () =>{
+    // window.open("https://drive.google.com/file/d/1LbT98fGmjkfihIQekSA-zXA9RotVXnS9/view", '_blank'); // group presentation
+    window.open("https://youtube-webapp-project.netlify.app/", '_blank'); // individual presentation
+}
+
+let dataMonitSrcBtn = document.querySelector("#Weather-App_source_code_btn");
+dataMonitSrcBtn.onclick = () =>{
+    window.open("https://github.com/Prashant5300/Weather-App-", '_blank');
+}
+
+let dataMonitLiveBtn = document.querySelector("#Weather-App_live_btn");
+dataMonitLiveBtn.onclick = () =>{
+    window.open("https://weather-webapp-project.netlify.app/", '_blank');
+}
+
+let zee5SrcBtn = document.querySelector("#zee5_source_code_btn");
+zee5SrcBtn.onclick = () =>{
+    window.open("https://github.com/Prashant5300/Zee5-clone", '_blank');
+}
+
+let zee5LiveBtn = document.querySelector("#zee5_live_btn");
+zee5LiveBtn.onclick = () =>{
+    window.open("https:zee5-clone-roshan.netlify.app//", '_blank');
+}
+
+// =====================================================================================================
+// ******************************* Removed this project **********************************
+
+// let brtlnSrcBtn = document.querySelector("#brightline_source_code_btn");
+// brtlnSrcBtn.onclick = () =>{
+//     window.open("https://github.com/Roshan-Patro/Brightline-Clone", '_blank');
+// }
+
+// let brtlnLiveBtn = document.querySelector("#brightline_live_btn");
+// brtlnLiveBtn.onclick = () =>{
+//     window.open("https://peppy-valkyrie-ddd5ab.netlify.app/", '_blank');
+// }
+// =====================================================================================================
